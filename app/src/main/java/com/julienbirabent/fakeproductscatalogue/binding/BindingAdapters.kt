11215a9949eede634e9f404f1b374652c42f2895
Julien Birabent <julien@sidekickinteractive.com>
2020-02-23 14:20:42 -0500

    Added extra databinding for image resources

--- app/src/main/java/com/julienbirabent/fakeproductscatalogue/binding/BindingAdapters.kt
+++ app/src/main/java/com/julienbirabent/fakeproductscatalogue/binding/BindingAdapters.kt
@@ -1,11 +1,16 @@
 package com.julienbirabent.fakeproductscatalogue.binding
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.julienbirabent.fakeproductscatalogue.data.entity.ImageResource
 import com.julienbirabent.fakeproductscatalogue.ui.LayoutManagerFactory
 
 object BindingAdapters {
@@ -32,5 +37,44 @@ fun bindRecyclerViewAdapter(
         if (disableAnimation) itemAnimator = null else DefaultItemAnimator()
         this.adapter = adapter
     }
+}
+
+@BindingAdapter("android:src")
+fun setImageResourceResId(imageView: ImageView, resource: Int) {
+    imageView.setImageResource(resource)
+}
+
+@BindingAdapter("android:src")
+fun setImageUri(view: ImageView, imageUri: String) {
+    Glide.with(view).load(imageUri).into(view)
+}
+
+@BindingAdapter("android:src")
+fun setImageUriNullable(view: ImageView, imageUri: String?) {
+    Glide.with(view).load(imageUri).into(view)
+}
+
+
+@BindingAdapter("android:src")
+fun setImageUri(view: ImageView, imageUri: Uri?) {
+    view.setImageURI(imageUri)
+}
+
+@BindingAdapter("android:src")
+fun setImageDrawable(view: ImageView, drawable: Drawable?) {
+    view.setImageDrawable(drawable)
+}
+
+@BindingAdapter("android:src")
+fun setImageResource(view: ImageView, imageResource: ImageResource<*>) {
+    when (imageResource.resource) {
+        is String -> setImageUri(view, imageResource.resource)
+        is Int -> setImageResourceResId(view, imageResource.resource)
+    }
+}
+
+@BindingAdapter("android:src")
+fun setImageResource(imageView: ImageView, resource: Int) {
+    imageView.setImageResource(resource)
 }
 
---
+++ app/src/main/java/com/julienbirabent/fakeproductscatalogue/data/entity/ImageResource.kt
@@ -0,0 +1,3 @@
+package com.julienbirabent.fakeproductscatalogue.data.entity
+
+class ImageResource<ResourceType>(val resource: ResourceType)

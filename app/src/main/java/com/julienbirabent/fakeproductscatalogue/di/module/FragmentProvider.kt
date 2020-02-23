3091422de9d0097dda1c7d059972ee1a339be7ec
Julien Birabent <julien@sidekickinteractive.com>
2020-02-23 17:04:06 -0500

    Forgot to add provide method to fragment provider

--- app/src/main/java/com/julienbirabent/fakeproductscatalogue/di/module/FragmentProvider.kt
+++ app/src/main/java/com/julienbirabent/fakeproductscatalogue/di/module/FragmentProvider.kt
@@ -1,5 +1,6 @@
 package com.julienbirabent.fakeproductscatalogue.di.module
 
+import com.julienbirabent.fakeproductscatalogue.ui.fragment.CatalogueListFragment
 import com.julienbirabent.fakeproductscatalogue.ui.fragment.MainFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -9,4 +10,7 @@ abstract class FragmentProvider {
 
     @ContributesAndroidInjector
     abstract fun contributeMainFragment() : MainFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeCatalogueListFragment() : CatalogueListFragment
 }

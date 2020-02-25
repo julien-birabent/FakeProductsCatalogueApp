296ef7d4afbfc559558f206e019662e18c58cb73
Julien Birabent <julien@sidekickinteractive.com>
2020-02-25 11:14:33 -0500

    Item product details layout for the wish list

---
+++ app/src/main/java/com/julienbirabent/fakeproductscatalogue/ui/item/ItemProductDetails.kt
@@ -0,0 +1,14 @@
+package com.julienbirabent.fakeproductscatalogue.ui.item
+
+import com.julienbirabent.fakeproductscatalogue.data.entity.ImageResource
+import com.julienbirabent.fakeproductscatalogue.ui.adapter.OmniAdapter
+
+class ItemProductDetails<T>(
+    var name: String? = "",
+    var imageResource: ImageResource<*>?,
+    var price: String? = "",
+    var shortDescription: String? = "",
+    var isOutOfStock: Boolean = false,
+    var colorAdapter: OmniAdapter,
+    extraData: T
+) : ItemHoldingExtraData<T>(extraData)
